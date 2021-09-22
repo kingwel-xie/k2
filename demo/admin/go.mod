0fea6aa4f8cf0c2b8e862d35263fba4fcb5c4181
kingwel <kingwel.xie@139.com>
2021-09-22 17:06:41 +0800

    more sample files in demo

---
+++ demo/admin/go.mod
@@ -0,0 +1,19 @@
+module admin
+
+go 1.15
+
+require (
+	github.com/appleboy/gin-jwt/v2 v2.6.4
+	github.com/gin-gonic/gin v1.7.4
+	github.com/google/uuid v1.3.0
+	github.com/kingwel-xie/k2 latest
+	github.com/prometheus/client_golang v1.11.0
+	github.com/spf13/cobra v1.2.1
+	github.com/swaggo/gin-swagger v1.3.1
+	github.com/swaggo/swag v1.7.1
+	golang.org/x/crypto v0.0.0-20210915214749-c084706c2272
+	gorm.io/driver/mysql v1.1.2 // indirect
+	gorm.io/gorm v1.21.15
+)
+
+// replace github.com/kingwel-xie/k2 latest => ../k2
--- demo/admin/main.go
+++ demo/admin/main.go
@@ -2,21 +2,25 @@ package main
 
 import (
 	"github.com/kingwel-xie/k2/cmd"
+	"github.com/kingwel-xie/k2/cmd/migrate"
+
+	"kx/entry"
+	_ "kx/migrate/version"
 )
 
 //go:generate swag init --parseDependency --parseDepth=6
 
-// @title Kobh Web Framework
+// @title KX Web Backend
 // @version 1.0.0
-// @description 基于Gin + Vue + Element UI的前后端分离权限管理系统的接口文档
-// @description KOBH
+// @description KX Web Backend
+// @description KX
 // @license.name MIT
-// @license.url https://github.com
+// @license.url https://github.com/...
 
 // @securityDefinitions.apikey Bearer
 // @in header
 // @name Authorization
 func main() {
-	cmd.Init("hsh")
+	cmd.Init("kx", entry.StartCmd, migrate.StartCmd)
 	cmd.Execute()
 }
