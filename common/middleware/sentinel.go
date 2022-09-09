c93061345017de509e804dd2632aaaf98cdf9e32
kingwel <kingwel.xie@139.com>
2022-09-09 16:56:49 +0800

    bump alibaba/sentinel to v1.0.4, pkg issue fix

--- common/middleware/sentinel.go
+++ common/middleware/sentinel.go
@@ -1,15 +1,53 @@
 package middleware
 
 import (
-	sentinelPlugin "github.com/alibaba/sentinel-golang/adapter/gin"
 	"github.com/alibaba/sentinel-golang/core/system"
 	"github.com/alibaba/sentinel-golang/logging"
+	sentinelPlugin "github.com/alibaba/sentinel-golang/pkg/adapters/gin"
 	"github.com/gin-gonic/gin"
+	"github.com/kingwel-xie/k2/core/logger"
 )
+
+type logAdapter struct {
+	*logger.KLogger
+}
+
+func (l logAdapter) Debug(msg string, keysAndValues ...interface{}) {
+	l.KLogger.Debugw(msg, keysAndValues...)
+}
+
+func (l logAdapter) DebugEnabled() bool {
+	return true
+}
+
+func (l logAdapter) Info(msg string, keysAndValues ...interface{}) {
+	l.KLogger.Infow(msg, keysAndValues...)
+}
+
+func (l logAdapter) InfoEnabled() bool {
+	return true
+}
+
+func (l logAdapter) Warn(msg string, keysAndValues ...interface{}) {
+	l.KLogger.Warnw(msg, keysAndValues...)
+}
+
+func (l logAdapter) WarnEnabled() bool {
+	return true
+}
+
+func (l logAdapter) Error(err error, msg string, keysAndValues ...interface{}) {
+	keysAndValues = append(keysAndValues, "Error", err)
+	l.KLogger.Errorw(msg, keysAndValues...)
+}
+
+func (l logAdapter) ErrorEnabled() bool {
+	return true
+}
 
 // Sentinel 限流
 func Sentinel() gin.HandlerFunc {
-	_ = logging.ResetGlobalLogger(log.WithCallerSkip(1))
+	_ = logging.ResetGlobalLogger(logAdapter{ log.WithCallerSkip(1) })
 	if _, err := system.LoadRules([]*system.Rule{
 		{
 			MetricType:   system.InboundQPS,
