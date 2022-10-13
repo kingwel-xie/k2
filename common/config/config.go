e1ade7da008fb28dda1f28d10b1cae880becac7e
likai1130 <15010609296@163.com>
2022-10-13 23:36:43 +0800

    add environment variable configuration

--- common/config/config.go
+++ common/config/config.go
@@ -3,6 +3,7 @@ package config
 import (
 	"fmt"
 	"github.com/spf13/viper"
+	"strings"
 )
 
 var (
@@ -73,16 +74,29 @@ func Setup(configFile string,
 		Extend:    ExtendConfig,
 		callbacks: fs,
 	}
+
 	v := viper.New()
+	//自动获取全部的env加入到viper中。默认别名和环境变量名一致。（如果环境变量多就全部加进来）
+	v.AutomaticEnv()
+
+	//替换读取格式。默认a.b.c.d格式读取env，改为a_b_c_d格式读取
+	v.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
+
+	// 本地配置文件位置
 	v.SetConfigFile(configFile)
+
+	//支持 "yaml", "yml", "json", "toml", "hcl", "tfvars", "ini", "properties", "props", "prop", "dotenv", "env":
 	v.SetConfigType("yaml")
+
+	//读配置文件到viper配置中
 	err := v.ReadInConfig()
 	if err != nil {
 		panic(fmt.Errorf("Fatal error config file: %s \n", err))
 	}
 
-	if err := v.Unmarshal(&_cfg); err != nil {
-		fmt.Println(err)
+	// 系列化成config对象
+	if err = v.Unmarshal(&_cfg); err != nil {
+		panic(err)
 	}
 
 	_cfg.Init()
